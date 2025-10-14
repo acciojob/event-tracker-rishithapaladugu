c9e816a2aa89f83b8c448feaec1b21e25d269917
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-14 09:50:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
+import React, { useState } from "react";
+import CalendarComponent from "./components/Calendar";
+import EventFilters from "./components/EventFilters";
+import "./styles/app.css";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [events, setEvents] = useState([]);
+  const [filter, setFilter] = useState("all"); // all | past | upcoming
 
-const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app">
+      <h1>Event Tracker</h1>
+      <EventFilters filter={filter} setFilter={setFilter} />
+      <CalendarComponent
+        events={events}
+        setEvents={setEvents}
+        filter={filter}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
